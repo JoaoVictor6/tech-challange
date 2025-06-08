70f1d0ed3a9596b3c4a8516b77bd08dcf1fb9cf9
João Victor <santosjoao.dev@gmail.com>
2025-06-08 18:23:16 -0300

    using a real mongo

--- apps/tagme-bff/src/app.module.ts
+++ apps/tagme-bff/src/app.module.ts
@@ -3,20 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsModule } from './items/items.module';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
-      useFactory: async () => {
-        const mongod = await MongoMemoryServer.create();
-        const uri = mongod.getUri();
-        return { uri };
-      },
-    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-crud'),
     ItemsModule,
   ],
-  controllers: [ AppController ],
-  providers: [ AppService ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
